765207fc42298a38a7bcf38f88cf32e783c9478e
123 <66757790+wozaibali@users.noreply.github.com>
2024-02-10 11:29:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-git clone https://github.com/Hyy2001X/AutoBuild-Packages/tree/master/luci-app-adguardhome
+#git clone https://github.com/Hyy2001X/AutoBuild-Packages/tree/master/luci-app-adguardhome
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
